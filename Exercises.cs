dd1d6fc69744a529053e042b4e15d8e8cc65fcab
George Honeywell <george.honeywell@live.co.uk>
2025-11-24 19:02:02 +0000

    Added Exercises.cs with a few fields to serve as the main class for managing different exercises.

---
+++ Exercises.cs
@@ -0,0 +1,9 @@
+﻿namespace HoneywellFitness
+{
+  internal class Exercises
+  {
+    public string Name { get; set; }
+    public int MaxWeightKG { get; set; }
+    public string BodyPart { get; set; }
+  }
+}
